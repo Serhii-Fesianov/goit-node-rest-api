1db2451ee031f5176f44b9c9379f2dde3fed020b
Serhii-Fesianov <littlefox06042000@gmail.com>
2024-03-29 18:55:16 +0200

    signup and signin with token

--- controllers/authControlers.js
+++ controllers/authControlers.js
@@ -1,20 +1,59 @@
+import {
+  createUser,
+  findUser,
+  updateUser,
+  validatePassword,
+} from "../auth-services/auth-services.js";
 import HttpError from "../helpers/HttpError.js";
-import { User } from "../models/User.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   try {
     const { email } = req.body;
-    const user = await User.findOne({ email: email });
+    const user = await findUser({ email: email });
 
     if (user) {
-      throw HttpError(409);
+      throw HttpError(409, "Email in use");
     }
 
-    const newUser = await User.create(req.body);
+    const newUser = await createUser(req.body);
 
     res.status(201).json({
       username: newUser.username,
       email: newUser.email,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const user = await findUser({ email: email });
+    if (!user) {
+      throw HttpError(401, "Email or password wrong");
+    }
+    const comparePassword = await validatePassword(password, user.password);
+
+    if (!comparePassword) {
+      throw HttpError(401, "Email or password wrong");
+    }
+
+    const { _id: id } = user;
+
+    const payload = {
+      id: id,
+    };
+
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+      expiresIn: "24h",
+    });
+
+    await updateUser({ _id: id }, { token });
+
+    res.json({
+      token: token,
     });
   } catch (error) {
     next(error);
